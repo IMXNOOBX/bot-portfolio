a06e3f90cad24ee82ab73f57e4119d3f7c389b76
IMXNOOBX <69653071+IMXNOOBX@users.noreply.github.com>
2024-04-08 21:12:17 +0200

    Implemented TOS and Privacy Policy required by discord

---
+++ src/routes/privacy-policy/page.ts
@@ -0,0 +1,22 @@
+export default {
+    title: "Privacy Policy",
+    description: "{bot_name} is a Discord bot designed to help manage emojis within Discord servers. This privacy policy outlines how {bot_name} ensures user privacy and will be refered as 'the bot'.",
+    sections: [
+      {
+        "title": "Data Collection and Usage",
+        "content": "The bot does not collect or store any user data. It operates solely within Discord's ecosystem and only interacts with data provided by Discord users in the course of managing emojis within Discord servers. The data is never stored."
+      },
+      {
+        "title": "Data Security",
+        "content": "The bot prioritizes the security of user data and takes all necessary measures to ensure that user data is handled securely and is never stored locally or externally."
+      },
+      {
+        "title": "User Rights",
+        "content": "Users have full control over the data they provide to the bot within their Discord servers. They are free to use the bot's features as they see fit, and can remove the bot from their server at any time."
+      },
+      {
+        "title": "Updates to Privacy Policy",
+        "content": "This privacy policy may be updated from time to time to reflect changes in how the bot operates or to comply with legal requirements. Users will be notified of any changes to the privacy policy."
+      }
+    ]
+};
