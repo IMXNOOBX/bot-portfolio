1b2a5c2de47ab8cf8eca356ca230784bc7cf8c08
IMXNOOBX <69653071+IMXNOOBX@users.noreply.github.com>
2024-04-08 21:12:17 +0200

    Implemented TOS and Privacy Policy required by discord

---
+++ src/routes/terms-of-service/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { onMount } from 'svelte';   
+    import config from '../../config.ts';
+    import page from './page.ts'
+
+</script>
+
+<!-- Watermak -->
+<div class="absolute z-10 top-0 xs:top-2 xl:bottom-2 xl:top-auto text-white w-full flex items-center justify-center gap-2 text-sm opacity-50">
+    Made with 🐱 by 
+    <a href="https://github.com/IMXNOOBX" target="_blank" rel="noopener noreferrer" class="font-semibold">@imxnoobx</a>
+</div>
+
+
+<!-- Main content -->
+<main class="text-white flex flex-col p-5 xs:p-10 md:px-32 md:py-20 xl:p-40 h-full w-full items-center justify-center gap-4 overflow-x-hidden">
+    <h1 class="text-6xl font-bold">{page.title}</h1>
+    <p class="text-zinc-400 text-center">{page.description.replaceAll('{bot_name}', config.name)}</p>
+    <div class="bg-zinc-800/50 border border-zinc-800 p-4 rounded-xl backdrop-blur-sm">
+        {#each page.sections as section}
+            <h2 class="text-base font-semibold">{section.title}</h2>
+            <p class="text-zinc-500 pl-2">{section.content}</p>
+        {/each}
+    </div>
+</main>
---
+++ src/routes/terms-of-service/page.ts
@@ -0,0 +1,18 @@
+export default {
+    title: "Terms of Service",
+    description: "These Terms of Service govern your use of {bot_name} Bot. By using {bot_name}, you agree to comply with these terms.",
+    sections: [
+      {
+        "title": "Discord Terms of Service",
+        "content": "Discord's Terms of Service at https://discord.com/terms."
+      },
+      {
+        "title": "Use of the Bot",
+        "content": "By using the bot, you agree to use it only in compliance with Discord's Terms of Service and Community Guidelines. You also agree not to use the bot for any illegal or unauthorized purposes."
+      },
+      {
+        "title": "Termination of Service",
+        "content": "The developers reserve the right to suspend or terminate access to the bot at any time, without prior notice or liability, for any reason whatsoever. The bot is maintained on a best-effort basis and may be discontinued at any time."
+      }
+    ]
+};
