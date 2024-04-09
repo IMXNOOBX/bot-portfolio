75d1f5ae4a2b93ea43b5f2f217144e70b7a1774c
IMXNOOBX <69653071+IMXNOOBX@users.noreply.github.com>
2024-04-09 13:02:22 +0200

    Added navigation to tos and pp pages and renamed data.ts and handled 404 errors

--- src/routes/privacy-policy/+page.svelte
+++ src/routes/privacy-policy/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     import { onMount } from 'svelte';   
     import config from '../../config.ts';
-    import page from './page.ts'
+    import page from './data.ts'
 
 </script>
 
@@ -13,13 +13,16 @@
 
 
 <!-- Main content -->
-<main class="text-white flex flex-col p-5 xs:p-10 md:px-32 md:py-20 xl:p-40 h-full w-full items-center justify-center gap-4 overflow-x-hidden">
-    <h1 class="text-6xl font-bold">{page.title}</h1>
-    <p class="text-zinc-400 text-center">{page.description.replaceAll('{bot_name}', config.name)}</p>
+<main class="text-white md:flex flex-col p-5 xs:p-10 md:px-32 md:py-20 xl:p-40 h-full w-full items-center justify-center gap-4 overflow-x-hidden">
+    <h1 class="text-4xl md:text-6xl font-bold text-center">{page.title}</h1>
+    <p class="text-zinc-400 text-center text-sm my-2 md:text-base">{page.description.replaceAll('{bot_name}', config.name)}</p>
     <div class="bg-zinc-800/50 border border-zinc-800 p-4 rounded-xl backdrop-blur-sm">
         {#each page.sections as section}
             <h2 class="text-base font-semibold">{section.title}</h2>
             <p class="text-zinc-500 pl-2">{section.content}</p>
         {/each}
+    </div>
+    <div class="w-full mt-4 flex items-center justify-center">
+        <a class="bg-zinc-700/50 py-1 px-2 border border-zinc-700 rounded-md z-10" href="/">Go Back</a>
     </div>
 </main>
